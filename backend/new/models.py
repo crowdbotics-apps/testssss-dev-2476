86faec268f1237b06bf6942a58b0755b0a2953ad
Crowdbotics <team@crowdbotics.com>
2020-04-08 14:47:52 +0000

    Update models

---
+++ backend/new/models.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.db import models
+
+
+class Test(models.Model):
+    "Generated Model"
+    test = models.BigIntegerField()
+
+
+# Create your models here.
--- backend/testssss_dev_2476/wsgi.py
+++ backend/testssss_dev_2476/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testssss_dev_2476.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testssss_dev_2476.settings")
 
 application = get_wsgi_application()
