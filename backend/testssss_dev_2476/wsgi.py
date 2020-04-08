ca040499ad2f101d73bc49da9cace8d9c0e1d263
Crowdbotics <team@crowdbotics.com>
2020-04-08 14:41:12 +0000

    Initial commit.

---
+++ backend/testssss_dev_2476/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testssss_dev_2476 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testssss_dev_2476.settings')
+
+application = get_wsgi_application()
---
+++ ios/testssss_dev_2476Tests/testssss_dev_2476Tests.m
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
+
+@interface testssss_dev_2476 : XCTestCase
+
+@end
+
+@implementation testssss_dev_2476
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
